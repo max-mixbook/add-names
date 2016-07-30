e5feaa92b4096d907978def2571501c0c475c039
Max <maksimshuster@Maksims-MacBook-Pro.local>
2016-07-29 21:28:13 -0700

    Refactored  as a revealing module

--- app.js
+++ app.js
@@ -1,74 +1,74 @@
-(function() {
+var app = (function() {
 
-    var app = {
-        
-        people: [],
-        
-        init: function(){
-            this.cacheDom();
-            this.bindEvents();
-            this.render();
-        },
-        
-        cacheDom: function(){
-            this.peopleModule = document.getElementById('peopleModule');
-            this.peopleList = document.getElementById('peopleList');
-            this.myInput = document.getElementById('myInput');
-            this.addButton = document.getElementById('addButton');
-        },
+    var people = [];
 
-        bindEvents: function() {
-            this.addButton.addEventListener("click", this.addPerson.bind(this));
-            this.peopleList.addEventListener("click", this.deletePerson.bind(this));
-        },
+    //cacheDOM 
+    var peopleModule = document.getElementById('peopleModule');
+    var peopleList = document.getElementById('peopleList');
+    var myInput = document.getElementById('myInput');
+    var addButton = document.getElementById('addButton');
+    
+    //bind events
+    addButton.addEventListener("click", addPerson.bind(this));
+    myInput.addEventListener("keydown", _handleEnter.bind(this));
+    peopleList.addEventListener("click", deletePerson.bind(this));
 
-        addPerson: function() {
-            if (this.myInput.value !== "") {
-                this.people.push(this.myInput.value);
-                this.render();
-                this.myInput.value = "";
-                console.log("button clicked");
-            } else {
-                return;
-            }
-           
-        },
+    _render();
 
-        deletePerson: function(e) {
-            if (e.target.className !== "delete") {
-                return;
-            } else {
-                var deleteTarget = e.target.parentNode;
-                var deleteIndex = Array.prototype.indexOf.call(this.peopleList.children, deleteTarget);
-                this.people.splice(deleteIndex, 1);
-                this.render();
-                console.log("name removed");
-            }
-        },
+    function _render(){
+        var data = people;
+        peopleList.innerHTML = "";
 
-        render: function(){
-            var data = this.people;
-            this.peopleList.innerHTML = "";
+        for (var k in data){
+            var newPerson = document.createElement('li');
+            var newPersonName = document.createElement('span');
+            var deleteBtn = document.createElement('span');
+            var deleteUI = deleteBtn.appendChild(document.createTextNode('x'));
 
-            for (var k in data){
-                var newPerson = document.createElement('li');
-                var newPersonName = document.createElement('span');
-                var deleteBtn = document.createElement('span');
-                var deleteUI = deleteBtn.appendChild(document.createTextNode('x'));
+            peopleList.appendChild(newPerson);
+            newPerson.appendChild(newPersonName);
+            newPersonName.appendChild(document.createTextNode(data[k]));
+            newPerson.appendChild(deleteBtn);
+            deleteBtn.className = "delete";
 
-                this.peopleList.appendChild(newPerson);
-                newPerson.appendChild(newPersonName);
-                newPersonName.appendChild(document.createTextNode(data[k]));
-                newPerson.appendChild(deleteBtn);
-                deleteBtn.className = "delete";
-   
             }       
+    }
+        
+    function _handleEnter(e) {
+        var key = e.which || e.keyCode;
+        if (key === 13) {
+            addPerson();
         }
+    }
+    
+    function addPerson(passedName) {
+        if (myInput.value !== "" || passedName !== "") {
+            var personName = (typeof passedName === "string") ? passedName : myInput.value;
+            people.push(personName);
+            _render();
+            myInput.value = "";
+            console.log("button clicked");
+        } else {
+            return;
+        }  
+    }
 
+    function deletePerson(e) {
+        if (e.target.className !== "delete") {
+            return;
+        } else {
+            var deleteTarget = e.target.parentNode;
+            var deleteIndex = Array.prototype.indexOf.call(this.peopleList.children, deleteTarget);
+            people.splice(deleteIndex, 1);
+            _render();
+            console.log("name removed");
+        }
+    }
+
+    return {
+        addPerson: addPerson,
+        deletePerson: deletePerson
     };
-
-    app.init();
-
 })()
   
   
